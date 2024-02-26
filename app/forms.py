94e8472ead9a557cbefeef819943b90d9684fd61
bryan <bryan@maynardfolks.com>
2024-02-26 17:59:33 -0500

    committing prior to trying to refactor for ui/api

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField
+from wtforms_alchemy import PhoneNumberField
+from wtforms.validators import Email, EqualTo, InputRequired, Length
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
+    username = StringField('Username:', validators=[InputRequired()])
+    password = PasswordField('Password:', validators=[InputRequired()])
+    remember_me = BooleanField('Remember Me:')
     submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    f_name = StringField('First Name:', validators=[InputRequired()])
+    l_name = StringField('Last Name:', validators=[InputRequired()])
+    email = EmailField('Email:', validators=[InputRequired(), Email()])
+
+    phone = PhoneNumberField('Phone:', validators=[InputRequired()], region="US", display_format='national')
+    
+    password = PasswordField('Password:', validators=[InputRequired(), EqualTo('confirm_pass', message="Passwords must match."), Length(min=8, message='Password must be at least 8 characters in length.')])
+    confirm_pass = PasswordField('Confirm Password:', validators=[InputRequired()])
+    submit = SubmitField('Sign Up')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for, request
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 
 @app.route('/')
 @app.route('/index')
@@ -12,8 +12,25 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
+        username = form.username.data.lower()
         flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data
+            username, form.remember_me.data
         ))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    registration_form = RegistrationForm()
+    if registration_form.validate_on_submit():
+        f_name = registration_form.f_name.data.capitalize()
+        l_name = registration_form.l_name.data.capitalize()
+        email = registration_form.email.data.lower()
+        flash('Registration successful for {} {}, with an email of {}, a phone number of {}, and a password of {}.'.format(
+            f_name, 
+            l_name, 
+            email, 
+            registration_form.phone.data, 
+            registration_form.password.data))
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Sign Up', form=registration_form)
