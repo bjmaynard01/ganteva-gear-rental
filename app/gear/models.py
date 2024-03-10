a28a6bd008ca52ea30e76087a27700bb3241e7e8
bryan <bryan@maynardfolks.com>
2024-03-09 22:22:07 -0500

    posted reddit question about templates, got gear/category relationships working in the meantime

--- app/gear/models.py
+++ app/gear/models.py
@@ -1,15 +1,23 @@
-from typing import Optional
+from typing import Optional, List
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
 from flask_marshmallow import Marshmallow
+from sqlalchemy.ext.declarative import declarative_base
 
 ma = Marshmallow()
+Base= declarative_base()
 
+gear_category = db.Table(
+    "gear_category",
+    db.Column('category_id', db.Integer, db.ForeignKey('gear_item.id'), primary_key=True),
+    db.Column('gear_id', db.Integer, db.ForeignKey('gear_categories.id'), primary_key=True)
+)
 class GearCategories(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     name: so.Mapped[str] = so.mapped_column(sa.String(80), index=True)
     description: so.Mapped[Optional[str]] = so.mapped_column(sa.String(200))
+    gear_items = so.relationship('GearItem', secondary=gear_category, backref='categories')
 
 class GearCategoriesSchema(ma.Schema):
     class Meta:
@@ -19,4 +27,10 @@ class GearCategoriesSchema(ma.Schema):
 class GearItem(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     name: so.Mapped[int] = so.mapped_column(sa.String(64), index=True)
+    image: so.Mapped[str] = so.mapped_column(sa.String(256), nullable=True)
+    care_instructions: so.Mapped[str] = so.mapped_column(sa.Text, nullable=True)
+    qty: so.Mapped[int] = so.mapped_column(default=0)
+    categories = so.relationship('GearCategories', secondary=gear_category, backref='gear_items')
+
+
 
